0c0ff6d5311bdafa2d8e8e82395573134c93ebbb
EBH <EBH@SHIHABTKD>
2023-10-16 10:32:11 +0300

    التحقق من حجم الملف

--- Models/Images.cs
+++ Models/Images.cs
@@ -8,6 +8,8 @@ namespace TestImages.Models
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Descrption { get; set; }
+
+        [Required(ErrorMessage ="الرجاء قم باختيار ملف ")]
         public string? ImageUrl { get; set; }
 
     }
